cb4eda648c9f199f24cbc7952b7ee07bf2732b60
Andromedde <88080821+Andromedde@users.noreply.github.com>
2023-05-04 11:11:54 +0400

    Update Lab3.cpp

--- Lab3.cpp
+++ Lab3.cpp
@@ -1,10 +1,10 @@
 #include "Vector.h"
 #include "Square.h"
 #include "Array.h"
+
 //2
 Vector::Vector(double x, double y)
-        : m_x(x), m_y(y)
-{}
+        : m_x(x), m_y(y) {}
 
 double Vector::getX() const {
     return m_x;
@@ -14,12 +14,12 @@ double Vector::getY() const {
     return m_y;
 }
 
-Vector Vector::operator+(const Vector& other) const {
+Vector Vector::operator+(const Vector &other) const {
     return Vector(m_x + other.getX(), m_y + other.getY());
 }
+
 Square::Square(double x, double y, double side, double angle)
-        : x(x), y(y), side(side), angle(angle)
-{}
+        : x(x), y(y), side(side), angle(angle) {}
 
 double Square::getX() const {
     return x;
@@ -45,31 +45,30 @@ void Square::increaseSide(double factor) {
     side *= factor;
 }
 
-void Square::moveByVector(const Vector& v) {
+void Square::moveByVector(const Vector &v) {
     x += v.getX();
     y += v.getY();
 }
 
-bool Square::operator==(const Square& other) const {
+bool Square::operator==(const Square &other) const {
     return abs(getArea() - other.getArea()) < 0.0001;
 }
 
-bool Square::operator!=(const Square& other) const {
+bool Square::operator!=(const Square &other) const {
     return !(*this == other);
 }
 
-bool Square::operator<(const Square& other) const {
+bool Square::operator<(const Square &other) const {
     return getArea() < other.getArea();
 }
 
-bool Square::operator>(const Square& other) const {
+bool Square::operator>(const Square &other) const {
     return getArea() > other.getArea();
 }
 
 //6
 Array::Array()
-        : size(0)
-{}
+        : size(0) {}
 
 void Array::add(int value) {
     if (size < MAX_SIZE) {
@@ -77,7 +76,7 @@ void Array::add(int value) {
     }
 }
 
-bool Array::operator==(const Array& other) const {
+bool Array::operator==(const Array &other) const {
     if (size != other.size) {
         return false;
     }
@@ -89,19 +88,19 @@ bool Array::operator==(const Array& other) const {
     return true;
 }
 
-bool Array::operator!=(const Array& other) const {
+bool Array::operator!=(const Array &other) const {
     return !(*this == other);
 }
 
-bool Array::operator<(const Array& other) const {
+bool Array::operator<(const Array &other) const {
     return size < other.size;
 }
 
-bool Array::operator>(const Array& other) const {
+bool Array::operator>(const Array &other) const {
     return size > other.size;
 }
 
-Array Array::operator+(const Array& other) const {
+Array Array::operator+(const Array &other) const {
     Array result;
     for (int i = 0; i < size; i++) {
         result.add(data[i]);
