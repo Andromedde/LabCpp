b74d2eff6cf5d19e7adaac0843bc4cde81ae1fc5
Andromedde <88080821+Andromedde@users.noreply.github.com>
2023-05-04 11:11:05 +0400

    Update App.h

--- App.h
+++ App.h
@@ -28,10 +28,14 @@ public:
             Vector v(5, 5);
             s2.moveByVector(v);
             cout << s2.getX() << ", " << s2.getY() << endl;
-            bool n1=(s1 == s2);bool n2=(s1 != s2); bool n3=(s1 < s2); bool n4=(s1 > s2);
-            if(n1==0 and n2==1 and n3==0 and n4==1 and s1.getSide()==10 and s2.getX()==5 and s2.getY()==5){
+            bool n1 = (s1 == s2);
+            bool n2 = (s1 != s2);
+            bool n3 = (s1 < s2);
+            bool n4 = (s1 > s2);
+            if (n1 == 0 and n2 == 1 and n3 == 0 and n4 == 1 and s1.getSide() == 10 and s2.getX() == 5 and
+                s2.getY() == 5) {
                 cout << "\nTest completed" << endl;
-            } else{
+            } else {
                 cout << "\nTest failed" << endl;
             }
 
@@ -52,13 +56,14 @@ public:
             cout << (a1 != a3) << endl;
             cout << (a1 < a3) << endl;
             cout << (a1 > a3) << endl;
-            if((a1 == a2)==0 and (a1 != a2)==1 and (a1 < a2)==0 and (a1 > a2)==1
-            and (a1 == a3)==0 and (a1 != a3)==1 and (a1 < a3)==1 and (a1 > a3)==0){
+            if ((a1 == a2) == 0 and (a1 != a2) == 1 and (a1 < a2) == 0 and (a1 > a2) == 1
+                and (a1 == a3) == 0 and (a1 != a3) == 1 and (a1 < a3) == 1 and (a1 > a3) == 0) {
                 cout << "\nTest completed" << endl;
-            } else{
+            } else {
                 cout << "\nTest failed" << endl;
             }
         }
     }
 };
+
 #endif //PROGA_LAB3_APP_H
