394d621cc70c5d968dcf95013c387d8c40fd08d6
Andromedde <88080821+Andromedde@users.noreply.github.com>
2023-05-04 11:01:10 +0400

    Update Lab3.h

--- Lab3.h
+++ Lab3.h
@@ -1,5 +1,5 @@
-#ifndef PROGA_LAB2_LAB3_H
-#define PROGA_LAB2_LAB3_H
+#ifndef PROGA_LAB3_VECTOR
+#define PROGA_LAB3_VECTOR
 #include <cmath>
 #include "vector"
 class Vector {
@@ -14,43 +14,4 @@ private:
     double m_x;
     double m_y;
 };
-class Square {
-public:
-    Square(double x, double y, double side, double angle);
-    double getX() const;
-    double getY() const;
-    double getSide() const;
-    double getAngle() const;
-    double getArea() const;
-    void increaseSide(double factor);
-    void moveByVector(const Vector& v);
-
-    bool operator==(const Square& other) const;
-    bool operator!=(const Square& other) const;
-    bool operator<(const Square& other) const;
-    bool operator>(const Square& other) const;
-
-private:
-    double x;
-    double y;
-    double side;
-    double angle;
-};
-class Array {
-public:
-    Array();
-    void add(int value);
-    bool operator==(const Array& other) const;
-    bool operator!=(const Array& other) const;
-    bool operator<(const Array& other) const;
-    bool operator>(const Array& other) const;
-    Array operator+(const Array& other) const;
-
-private:
-    static const int MAX_SIZE = 100;
-    int size;
-    int data[MAX_SIZE];
-};
-
-
-#endif //PROGA_LAB2_LAB3_H
+#endif //PROGA_LAB3_VECTOR
