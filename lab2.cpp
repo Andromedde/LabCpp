c588aa8542a2736ab3f252a27340ce2412172fd2
Andromedde <88080821+Andromedde@users.noreply.github.com>
2023-05-04 10:56:48 +0400

    Update lab2.cpp

--- lab2.cpp
+++ lab2.cpp
@@ -1,41 +1,48 @@
 #include "iostream"
 #include "fstream"
 #include "string"
+
 using namespace std;
 ofstream file;
-void open(string fileName){
+
+void open(string fileName) {
     file.open(fileName);
 }
-void close(string fileName){
+
+void close(string fileName) {
     file.close();
 }
-bool chek(string fileName){
-    if(file.is_open())
+
+bool chek(string fileName) {
+    if (file.is_open())
         return 1;
     else
         return 0;
 }
-void fileNameOut(string fileName){
+
+void fileNameOut(string fileName) {
     cout << "Name file: " << fileName << endl;
 }
-void addStr(string name, string str){
-    if (file.is_open()){
+
+void addStr(string name, string str) {
+    if (file.is_open()) {
         file << str << endl;
     }
 }
 
-void addNum(string name, int num){
-    if (file.is_open()){
+void addNum(string name, int num) {
+    if (file.is_open()) {
         file << num << endl;
     }
 }
 
-void addRnum(string name, float r_num){
-    if (file.is_open()){
+void addRnum(string name, float r_num) {
+    if (file.is_open()) {
         file << r_num << endl;
     }
 }
-void fileOut(string name, string str, int num, float r_num){
+
+void fileOut(string name, string str, int num, float r_num) {
     cout << "Test string: " << str << endl;
     cout << "Test number: " << num << endl;
     cout << "Test real number: " << r_num << endl;
