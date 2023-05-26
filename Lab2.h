21cf13b5f5d12565f754a139f544eed8cf3b07eb
Andromedde <88080821+Andromedde@users.noreply.github.com>
2023-05-26 21:12:36 +0400

    Update Lab2.h

--- Lab2.h
+++ Lab2.h
@@ -3,13 +3,23 @@
 #include "iostream"
 
 using namespace std;
+namespace Servis{
+
 void open(string fileName);
+
 void close(string fileName);
+
 bool chek(string fileName);
+
 void fileNameOut(string fileName);
+
 void addStr(string name, string str);
+
 void addNum(string name, int num);
+
 void addRnum(string name, float r_num);
+
 void fileOut(string name, string str, int num, float r_num);
 
+}
 #endif
