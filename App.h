fa378db525b2289ac8f96c3acf8b4d9f20f0aa10
Andromedde <88080821+Andromedde@users.noreply.github.com>
2023-05-26 21:12:22 +0400

    Update App.h

--- App.h
+++ App.h
@@ -1,40 +1,40 @@
 #ifndef PROGA_LAB2_APP_H
 #define PROGA_LAB2_APP_H
-
 #include "Lab2.h"
 #include <iostream>
 #include <string>
 
 using namespace std;
+namespace Servis {
 
-class App {
-public:
-    void Start() {
-        string str = "This is a test string!";
-        int num = 12345;
-        float r_num = 3.14159;
-        string fileName = "test.txt";
+    class App {
+    public:
+        void Start() {
+            string str = "This is a test string!";
+            int num = 12345;
+            float r_num = 3.14159;
+            string fileName = "test.txt";
 
-        open(fileName);
-        if (chek(fileName)) {
-            cout << "File is open\n" << endl;
-        } else {
-            cout << "File is close\n" << endl;
+            open(fileName);
+            if (chek(fileName)) {
+                cout << "File is open\n" << endl;
+            } else {
+                cout << "File is close\n" << endl;
+            }
+
+            addStr(fileName, str);
+            addNum(fileName, num);
+            addRnum(fileName, r_num);
+
+            fileNameOut(fileName);
+            fileOut(fileName, str, num, r_num);
+            close(fileName);
+            if (chek(fileName)) {
+                cout << "\nFile is open" << endl;
+            } else {
+                cout << "\nFile is close" << endl;
+            }
         }
-
-        addStr(fileName, str);
-        addNum(fileName, num);
-        addRnum(fileName, r_num);
-
-        fileNameOut(fileName);
-        fileOut(fileName, str, num, r_num);
-        close(fileName);
-        if (chek(fileName)) {
-            cout << "\nFile is open" << endl;
-        } else {
-            cout << "\nFile is close" << endl;
-        }
-    }
-};
-
+    };
+}
 #endif //PROGA_LAB2_APP_H
